153eaa7660e1294aee67ceb65e904461b86b413a
João Lucas de Carvalho Arruda <jlcarruda3@gmail.com>
2019-10-08 04:18:50 -0300

    implemented initial models test configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src } = require('gulp');
 const jshint = require('gulp-jshint');
+const jest = require('gulp-jest').default;
 const stylish = require('jshint-stylish');
 const app = require('./app');
 const pkg = require('./package');
@@ -11,6 +12,11 @@ exports.lint = () => {
     .pipe(jshint(pkg.jshintConfig))
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
+};
+
+exports.test = () => {
+  return src('tests/**/*.test.js')
+    .pipe(jest(pkg.jest));
 };
 
 exports.build = (cb) => {
